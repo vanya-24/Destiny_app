5e334e539ee4fedb8d687d8cfe36c02347895114
Vanya Arora <vanyaarora419@gmail.com>
2020-09-30 21:13:24 +0530

    Destiny App

---
+++ lib/story.dart
@@ -0,0 +1,14 @@
+class Story {
+  String  storytitle;
+  String choice_1;
+  String choice_2;
+
+  Story({String storyTitle,String choice1,String choice2}){
+    storytitle=storyTitle;
+    choice_1=choice1;
+    choice_2=choice2;
+  }
+
+
+}
+
